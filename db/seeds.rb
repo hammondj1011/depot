939adfdd3c10538cd9fd2f46f4c556b63bd1d920
Jeff Hammond <jhammond6@brookdale.com>
2018-09-12 16:32:14 -0500

    Chapter 6, Task A: mostly complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+#...
+Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
+	description: %{<p>stuff goes here</p>},
+	image_url: '7apps.jpg',
+	price: 26.00)	
+#...
+Product.create!(title: 'I\'m just a product',
+	description: %{<p>stuff goes here</p>},
+	image_url: '7apps.jpg',
+	price: 15.00)	
+#...
+Product.create!(title: 'Learn Ruby Stuff Today',
+	description: %{<p>stuff goes here</p>},
+	image_url: '7apps.jpg',
+	price: 5.50)	
+#...
+Product.create!(title: 'Uncle Matt',
+	description: %{<p>stuff goes here</p>},
+	image_url: '7apps.jpg',
+	price: 19.95)	
+#...
+
+
