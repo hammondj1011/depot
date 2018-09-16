02a9243a21c5107e1b8f04212a006d5594b25a84
hammondj1011 <hammondj1011@gmail.com>
2018-09-16 08:59:56 -0500

    view work

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  resources :products
-  
-  get 'welcome/index'
+  root 'store#index', as: 'store_index'
 
-  root 'products#index'
+  resources :products
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
